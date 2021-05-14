a0c42abdd620671fa5544e3b03f489a45148d27d
Vagner de Souza <inaciovagner1@gmail.com>
2021-05-14 16:05:57 +0100

    Add project files.

---
+++ Using_Object/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_Object
+{
+    public class Employee
+    {
+        public int id;
+        public String name;
+    }
+    class EmployeeDetail
+    {
+        public static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.id = 20;
+            emp.name = "John Smith";
+            Console.WriteLine(emp.id);
+            Console.WriteLine(emp.name);
+        }
+    }
+}
